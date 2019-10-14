c389ecf28b7b5af42ee164a29234e4cc7f04f4a0
Aaron Ulate <aulate@localhost.localdomain>
2019-10-13 20:25:04 -0600

    agregar ingrediente y actualizar ingrediente

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
 import {ShoppingListService} from './shopping-list.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
@@ -9,18 +10,28 @@ import {ShoppingListService} from './shopping-list.service';
   styleUrls: ['./shopping-list.component.scss'],
   providers: [ShoppingEditComponent]
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredientes: Ingredient[];
+  private subscription: Subscription;
+
   constructor( private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredientes = this.slService.getIngredients();
-    this.slService.ingredientChanged
+    this.subscription =  this.slService.ingredientChanged
       .subscribe(
         (ingredients: Ingredient[]) => {
           this.ingredientes = ingredients;
         }
       );
+  }
+
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,10 @@
 import { EventEmitter } from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {Subject} from 'rxjs';
 
 export class ShoppingListService {
   ingredientChanged = new EventEmitter<Ingredient[]>();
+  startedEditing = new Subject<number>();
   private ingredientes: Ingredient[] = [
     new Ingredient('Manzanas', 5),
     new Ingredient('Tomates', 10)
@@ -10,6 +12,10 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredientes.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredientes[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -20,5 +26,10 @@ export class ShoppingListService {
   addIngredients(ingredients: Ingredient[]) {
     this.ingredientes.push(...ingredients);
     this.ingredientChanged.emit(this.ingredientes.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredientes[index] = newIngredient;
+    this.ingredientChanged.next(this.ingredientes.slice());
   }
 }
